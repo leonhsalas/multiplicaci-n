2a748b7c2df3d8e1a2ac56112f8afc501dcf4e93
Leon Nicolhas <leonnhsalas@gmail.com>
2023-12-01 09:36:42 -0600

    second commit

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-numeroparamultiplicar = int(input("Cual Tabla de multiplicar quieres"))
+numeroParaMultiplicar = int(input("¿Cual Tabla de multiplicar quieres? "))
 
+contador = 0
+
+while contador < 13:
+    resultado = numeroParaMultiplicar * contador
+    print(f"{numeroParaMultiplicar} x {contador} = {resultado}")
+    contador = contador + 1
